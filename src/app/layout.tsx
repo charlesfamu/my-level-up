718dfc0ec162ecf7bc6a0bf30018379236dc97ab
Charles Bunton <cbunton2@gmail.com>
2024-10-29 11:25:55 -0400

    updated the copy in the footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
               className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t h-16"
             >
               <p className="text-xs text-muted-foreground">
-                &copy; 2024 A Level Up. All rights reserved.
+                &copy; 2024 My Level Up. All rights reserved.
               </p>
             </footer>
           </div>
